b715a89c35c2a4cd132e6efb88de3a4f7c1ef6de
Praveen Ravichandran <me@pravinba9495.dev>
2022-01-05 00:16:49 -0500

    Switch log level from env var to a local var

--- helper.go
+++ helper.go
@@ -1,32 +1,27 @@
 package logger
 
-import "os"
+var (
+	// Default log Level
+	LOG_LEVEL = "ERROR"
+)
 
 // SetLogLevel sets the log level
 func SetLogLevel(level string) {
-	os.Setenv("LOG_LEVEL", level)
+	LOG_LEVEL = level
 }
 
 // GetLogLevel returns the set log level
 func GetLogLevel() int {
-	level, bool := os.LookupEnv("LOG_LEVEL")
-	if bool {
-		switch level {
-		case "TRACE":
-			return 0
-		case "LOG":
-			return 1
-		case "WARN":
-			return 2
-		case "ERROR":
-			return 3
-		default:
-			return 3
-		}
-	} else {
-		if err := os.Setenv("LOG_LEVEL", "ERROR"); err != nil {
-			return 3
-		}
-		return GetLogLevel()
+	switch LOG_LEVEL {
+	case "TRACE":
+		return 0
+	case "LOG":
+		return 1
+	case "WARN":
+		return 2
+	case "ERROR":
+		return 3
+	default:
+		return 3
 	}
 }
